152ec85b3c108852c6f74c98d3922aec4bdc5cca
Chukwu Stanley <stanleychukwu17@yahoo.com>
2023-08-29 15:45:00 +0100

    done for the day, started the hero section, will round the project up tomorrow

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import './index.css'
+import Hero from './sections/Hero'
 import Nav from './sections/Nav'
 
 function App() {
     return (
         <main className="">
             <Nav />
+            <section>
+                <Hero />
+            </section>
         </main>
     )
 }
---
+++ src/sections/Hero.tsx
@@ -0,0 +1,24 @@
+
+export default function Hero() {
+    return (
+        <section className="relative padding-x py-5 text-base">
+            <div className="">
+                <div className="pt-12">Our summer collection</div>
+                <header className="text-8xl font-medium py-5 leading-[1.25]">
+                    <p>The new arrival</p>
+                    <p>Nike shoes</p>
+                </header>
+                <div className="">
+                    Discover stylish nike arrivals, quality comfort, and innovation for your active life
+                </div>
+                <div className="">
+                    <button>Shop now</button>
+                </div>
+                <div className="">
+                    statistics
+                </div>
+            </div>
+            <div className=""></div>
+        </section>
+    )
+}
