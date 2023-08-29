c7499d83bce1f80ad560b2ba32830c7c538aba26
Chukwu Stanley <stanleychukwu17@yahoo.com>
2023-08-29 13:35:22 +0100

    done with the header

--- src/sections/Nav.tsx
+++ src/sections/Nav.tsx
@@ -1,19 +1,23 @@
 import header_logo from '../assets/images/header-logo.svg'
+import hamburger from '../assets/icons/hamburger.svg'
 
 export default function Nav() {
     return (
         <header className="relative padding-x py-4 flex justify-between items-center">
-            <nav>
+            <nav className='max-md:w-28'>
                 <a href=""><img src={header_logo} alt="" /></a>
             </nav>
-            <nav className='flex justify-evenly gap-10 font-medium'>
-                <div className=""><a href="" className="">Home</a></div>
-                <div className=""><a href="" className="">About us</a></div>
-                <div className=""><a href="" className="">Products</a></div>
-                <div className=""><a href="" className="">Contact us</a></div>
+            <nav className='flex justify-evenly gap-12 font-medium max-md:hidden max-lg:text-sm max-lg:gap-10'>
+                <div><a href="">Home</a></div>
+                <div><a href="">About us</a></div>
+                <div><a href="">Products</a></div>
+                <div><a href="">Contact us</a></div>
             </nav>
-            <div className='font-semibold text-my-green text-sm bg-[#332d34] p-2.5 px-8 rounded-full tracking-wider'>
+            <div className='font-semibold text-my-green text-sm bg-[#332d34] p-2.5 px-8 rounded-full tracking-wider max-md:hidden'>
                 Sign In
+            </div>
+            <div className="w-7 hidden max-md:block">
+                <img src={hamburger} alt="" className=""/>
             </div>
         </header>
     )
