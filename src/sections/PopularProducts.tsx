d7d527f7df468adaa02722f0a3fe9074b537427a
Chukwu Stanley <stanleychukwu17@yahoo.com>
2023-09-02 22:32:14 +0100

    almost there for the popular products to be displayed

--- src/sections/PopularProducts.tsx
+++ src/sections/PopularProducts.tsx
@@ -13,12 +13,11 @@ export default function PopularProducts() {
         <section className="padding-x mt-40">
             <div className="text-4xl font-semibold">Our <span className="coGreen"> Popular </span>Products</div>
             <div className="py-6 w-[600px] text-lg leading-relaxed">Experience top-notch quality and style with our sought-after selections. Discover a world of comfort, design, and value</div>
-            <div className="">
-
+            <div className="flex justify-evenly">
                 {thumbnailImages.map((item) => {
                     return (
                         <div className="">
-                            <div  className="thumb w-[300px] h-[300px] bg-[#efeff0] border-2 rounded-lg p-1 hover:border-[#a3a0a9] cursor-pointer" key={item.id}>
+                            <div  className="w-[300px] h-[300px] rounded-lg cursor-pointer" key={item.id}>
                                 <img className='w-full h-full object-contain' src={item.src} alt="" />
                             </div>
                             <div className="">ranking</div>
