761f3a330af21f99eed84c939f47a448c5a1e275
Chukwu Stanley <stanleychukwu17@yahoo.com>
2023-09-06 18:27:57 +0100

    installed a package for generating random users-id finished the specialOffers section

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,15 +1,32 @@
+import { useEffect, useRef } from 'react'
+import { v4 as generateId } from 'uuid';
+import { gsap } from 'gsap';
+
 import {chevronRight} from '../assets/icons/index.ts'
 
 type buttonProps = {
     showICon?: 'hide'|'show'
+    bgColor?: string
+    textShow?: string
+    border?: string
 }
 
-export default function Button({showICon}: buttonProps) {
+export default function Button({showICon, bgColor, textShow, border}: buttonProps) {
+    const btnId = useRef<string>(generateId())
     const icon = showICon || 'show'
+    const text = textShow || 'Shop now'
+    const bg = bgColor || 'bg-coral-red'
+
+    useEffect(() => {
+        if (bg) {
+            const item = document.getElementById(btnId.current)
+            gsap.set(item, {'background':bgColor, border:border})
+        }
+    }, [])
 
     return (
-        <button className="bg-coral-red py-3 px-6 rounded-full font-semibold">
-            Shop now
+        <button id={btnId.current} className="relative bg-coral-red py-3 px-6 rounded-full font-semibold active:top-1">
+            {text}
             {icon === 'show' && (
                 <img className='inline w-6 ml-3' src={chevronRight} alt=" right arrow" />
             )}
--- src/sections/SpecialOffer.tsx
+++ src/sections/SpecialOffer.tsx
@@ -10,16 +10,21 @@ export default function SpecialOffer() {
             <div className="hidden max-lg:block text-center mt-10">
                 <Button />
             </div>
-            <div className="w-2/5 mt-4 max-lg:w-full max-lg:text-center max-lg:mb-10">
+            <div className="w-2/5 mt-6 max-lg:w-full max-lg:text-center max-lg:mb-10">
                 <header className="text-[65px] font-semibold leading-tight [word-spacing:5px] tracking-wide max-lg:text-4xl max-lg:font-bold">
                     We have <span className="coGreen">Special offers</span> for you!
                 </header>
                 <div className="text-xl leading-normal max-lg:text-lg max-lg:leading-relaxed">
-                    <p className='mt-7'>Embark on a shopping journey that redefines your experience with unbeatable deals. From premier selections to incredible savings, we offer unparalleled value that sets us apart.</p>
-                    <p className='mt-7'>Navigate a realm of possibilities designed to fulfill your unique desires, surpassing the loftiest expectations. Your journey with us is nothing short of exceptional.</p>
+                    <p className='mt-9'>Embark on a shopping journey that redefines your experience with unbeatable deals. From premier selections to incredible savings, we offer unparalleled value that sets us apart.</p>
+                    <p className='mt-9'>Navigate a realm of possibilities designed to fulfill your unique desires, surpassing the loftiest expectations. Your journey with us is nothing short of exceptional.</p>
                 </div>
-                <div className="mt-10 max-lg:hidden">
-                    <Button />
+                <div className="mt-12 flex space-x-10 max-lg:hidden">
+                    <div className="">
+                        <Button textShow='View details' showICon='hide' />
+                    </div>
+                    <div className="">
+                        <Button textShow='Learn more' showICon='hide' bgColor='white' border='1px solid #000' />
+                    </div>
                 </div>
             </div>
         </section>
