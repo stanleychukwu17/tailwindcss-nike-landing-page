eca04ec8e2b352a1985567fc0d77f708f3efe0d8
Chukwu Stanley <stanleychukwu17@yahoo.com>
2023-09-06 01:27:54 +0100

    brought in the icons for all of the 3 cards, now also displaying all the cards

--- src/sections/Services.tsx
+++ src/sections/Services.tsx
@@ -1,6 +1,39 @@
+import {truckFast, shieldTick, support} from '../assets/icons/index.ts'
+
+
+const whatIDo: {icon:string, title:string, content:string}[] = [
+    {
+        icon: truckFast,
+        title:'Free shipping',
+        content: 'Enjoy seamless shopping with our complimentary shipping service.'
+    },
+    {
+        icon: shieldTick,
+        title:'Secure Payment',
+        content: 'Experience worry-free transactions with our secure payment options.'
+    },
+    {
+        icon: support,
+        title:'Love to help you',
+        content: 'Our dedicated team is here to assist you every step of the way.'
+    },
+]
+console.log(whatIDo)
 
 export default function Services() {
     return (
-        <div>Services</div>
+        <section className="flex justify-evenly mb-40">
+            {whatIDo.map(item => {
+                return (
+                    <div className="">
+                        <div className="w-80 bg-black">
+                            <img src={item.icon} alt="" />
+                        </div>
+                        <div className="">{item.title}</div>
+                        <div className="">{item.content}</div>
+                    </div>
+                )
+            })}
+        </section>
     )
 }
